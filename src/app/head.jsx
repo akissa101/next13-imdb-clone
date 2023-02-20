9d2d280f3b56d04a3923fb77dfcc3efc23d7392f
Abdikarim Issa <kareemorbit@gmail.com>
2023-02-20 14:03:23 +0300

    Install next js 13

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDB Clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="IMDB clone by Abdikarim Issa" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
